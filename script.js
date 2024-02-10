7c5f6e48f14a4a77a052c48b4b8a6655941c9066
obivage <obivage99@gmail.com>
2024-02-11 00:20:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// document
+//   .getElementById("nav_hero-sgn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     document.getElementById("landing-page-content").classList.add("hidden");
+//     document.getElementById("section__sign-up").classList.remove("hidden");
+//   });
